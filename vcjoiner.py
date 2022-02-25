b6c838c8f7b2034ff8b7ce453dd64c08efc58f48
Mrunal <80758264+msy1717@users.noreply.github.com>
2022-02-25 17:24:34 +0530

    Add files via upload

---
+++ vcjoiner.py
@@ -0,0 +1,272 @@
+from telethon.sync import TelegramClient
+from telethon.tl.types import InputPeerChannel
+from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError, PhoneNumberBannedError, ChatAdminRequiredError
+from telethon.errors.rpcerrorlist import ChatWriteForbiddenError, UserBannedInChannelError, UserAlreadyParticipantError, FloodWaitError
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.functions.messages import ReportSpamRequest
+from telethon.tl.types import InputPeerEmpty
+from telethon.tl.functions.channels import LeaveChannelRequest
+import sys
+from pytgcalls import GroupCallFactory
+from telethon.tl.functions.photos import UploadProfilePhotoRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest, AddChatUserRequest
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.types import UserStatusRecently
+import time
+import random
+import os
+import pickle
+from telethon.sync import TelegramClient
+from telethon.errors.rpcerrorlist import PhoneNumberBannedError
+from colorama import init, Fore
+from time import sleep
+import csv
+scam = '@notoscam'
+init()
+
+n = Fore.RESET
+lg = Fore.LIGHTGREEN_EX
+r = Fore.RED
+w = Fore.WHITE
+cy = Fore.CYAN
+ye = Fore.YELLOW
+colors = [lg, r, w, cy, ye]
+
+try:
+    import requests
+except ImportError:
+    print(f'{lg}[i] Installing module - requests...{n}')
+    os.system('pip install requests')
+
+
+def countdown(t): 
+    while t: 
+        mins, secs = divmod(t, 60) 
+        timer = '{:02d}:{:02d}'.format(mins, secs) 
+        print(timer, end="\r") 
+        time.sleep(1) 
+        t -= 1
+    print('Fire in the hole!!') 
+t = 50
+# function call 
+
+
+
+def banner():
+    import random
+    # fancy logo
+
+   #kanger ki maki chut
+   # bsdk remove mt krna nhi toh gey ki aulad
+   # puch lena bsdkk                                                
+    
+    b= [
+
+     '██████╗ ███████╗ █████╗ ███████╗████████╗',
+     '██╔══██╗██╔════╝██╔══██╗██╔════╝╚══██╔══╝',
+     '██████╔╝█████╗  ███████║███████╗   ██║',
+     '██╔══██╗██╔══╝  ██╔══██║╚════██║   ██║',
+     '██████╔╝███████╗██║  ██║███████║   ██║',
+     '╚═════╝ ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝'
+    ]
+    for char in b:
+        print(f'{random.choice(colors)}{char}')
+    print('=============Dev-@Godmrunal==============')
+    print(f'{lg}Version: 1.2{lg} | Author: github.com/msy1717\n')
+def clr():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+while True:
+    clr()
+    banner()
+    print(ye+'Choose a Option:'+n)
+    print(cy+'            [1] Add new accounts'+n)
+    print(cy+'            [2] Remove specific accounts'+n)
+    print(cy+'            [3] Filter Banned Account'+n)
+    print(cy+'            [4] join VC'+n)
+    
+
+    a = int(input('\nEnter your choice: '))
+    if a == 1:
+        new_accs = []
+        with open('vars.txt', 'ab') as g:
+            number_to_add = int(input(f'\n{lg} [~] Enter number of accounts to add: {r}'))
+            for i in range(number_to_add):
+                phone_number = str(input(f'\n{lg} [~] Enter Phone Number: {r}'))
+                parsed_number = ''.join(phone_number.split())
+                pickle.dump([parsed_number], g)
+                new_accs.append(parsed_number)
+            print(f'\n{lg} [i] Saved all accounts in vars.txt')
+            clr()
+            print(f'\n{lg} [*] Logging in from new accounts\n')
+            for number in new_accs:
+                c = TelegramClient(f'sessions/{number}', 8027196 , '9b70b20efd67e9b99edc395d78407cfa')
+                c.start(number)
+                print(f'{lg}[+] Login successful')
+                c.disconnect()
+            input(f'\n Press enter to goto main menu...')
+
+        g.close()
+
+    elif a == 2:
+        accs = []
+        f = open('vars.txt', 'rb')
+        while True:
+            try:
+                accs.append(pickle.load(f))
+            except EOFError:
+                break
+        f.close()
+        i = 0
+        print(f'{lg}[i] Choose an account to delete\n')
+        for acc in accs:
+            print(f'{lg}[{i}] {acc[0]}{n}')
+            i += 1
+        index = int(input(f'\n{lg}[+] Enter a choice: {n}'))
+        phone = str(accs[index][0])
+        session_file = phone + '.session'
+        if os.name == 'nt':
+            os.system(f'del sessions\\{session_file}')
+        else:
+            os.system(f'rm sessions/{session_file}')
+        del accs[index]
+        f = open('vars.txt', 'wb')
+        for account in accs:
+            pickle.dump(account, f)
+        print(f'\n{lg}[+] Account Deleted{n}')
+        input(f'\nPress enter to goto main menu...')
+        f.close()
+
+    elif a == 3:
+        accounts = []
+        banned_accs = []
+        h = open('vars.txt', 'rb')
+        while True:
+            try:
+                accounts.append(pickle.load(h))
+            except EOFError:
+                break
+        h.close()
+        if len(accounts) == 0:
+            print(r+'[!] There are no accounts! Please add some and retry')
+            sleep(3)
+        else:
+            for account in accounts:
+                phone = str(account[0])
+                client = TelegramClient(f'sessions/{phone}', 8027196 , '9b70b20efd67e9b99edc395d78407cfa')
+                client.connect()
+                if not client.is_user_authorized():
+                    try:
+                        client.send_code_request(phone)
+                        #client.sign_in(phone, input('[+] Enter the code: '))
+                        print(f'{lg}[+] {phone} is not banned{n}')
+                    except PhoneNumberBannedError:
+                        print(r+str(phone) + ' is banned!'+n)
+                        banned_accs.append(account)
+            if len(banned_accs) == 0:
+                print(lg+'Congrats! No banned accounts')
+                input('\nPress enter to goto main menu...')
+            else:
+                for m in banned_accs:
+                    accounts.remove(m)
+                with open('vars.txt', 'wb') as k:
+                    for a in accounts:
+                        Phone = a[0]
+                        pickle.dump([Phone], k)
+                k.close()
+                print(lg+'[i] All banned accounts removed'+n)
+                input('\nPress enter to goto main menu...')
+
+
+    elif a == 4:
+        accounts = []
+        f = open('vars.txt', 'rb')
+        while True:
+            try:
+                accounts.append(pickle.load(f))
+            except EOFError:
+                break
+
+        
+        #print('\n' + info + lg + ' Checking for banned accounts...' + rs)
+        print('\n' ' Checking for banned accounts...' )
+        for a in accounts:
+            phn = a[0]
+            print(f'Checking {lg}{phn}')
+            clnt = TelegramClient(f'sessions/{phn}', 8027196 , '9b70b20efd67e9b99edc395d78407cfa')
+            clnt.connect()
+            banned = []
+            if not clnt.is_user_authorized():
+                try:
+                    clnt.send_code_request(phn)
+                    print('kk')
+                except PhoneNumberBannedError:
+                    print(f'{phn} {r}is banned!')
+                    banned.append(a)
+            for z in banned:
+                accounts.remove(z)
+                print('{lg}Banned account removed[Remove permanently using manager.py]{rs}')
+            time.sleep(0.5)
+            clnt.disconnect()
+        print(' Sessions created!')
+        clr()
+        banner()
+# func to log scraping details(link of the grp to scrape
+# and current index) in order to resume late    
+# load all the accounts(phonenumbers)
+        accounts = []
+        f = open('vars.txt', 'rb')
+        while True:
+            try:
+                accounts.append(pickle.load(f))
+            except EOFError:
+                break
+
+        print(f'{lg} Total accounts: {w}{len(accounts)}')
+        number_of_accs = int(input(f'{cy} Enter number of accounts to use: {r}'))
+
+        chat = str(input(f'send group username to join vc'))
+
+
+
+        print(f'_'*50)
+        to_use = [x for x in accounts[:number_of_accs]]
+        for l in to_use: accounts.remove(l)
+        with open('vars.txt', 'wb') as f:
+            for a in accounts:
+                pickle.dump(a, f)
+            for ab in to_use:
+                pickle.dump(ab, f)
+            f.close()
+        print(f'{lg} Joining Vc from {w}{number_of_accs} accounts...')
+        print(f'-'*50)
+        index = 0
+        for acc in to_use:
+            stop = index + 60
+            c = TelegramClient(f'sessions/{acc[0]}', 8027196 , '9b70b20efd67e9b99edc395d78407cfa')
+            print(f' User: {cy}{acc[0]}{lg} -- {cy}Starting session... ')
+            c.start(acc[0])
+            acc_name = c.get_me().first_name
+            
+            try:
+                result = client.invoke(functions.phone.JoinGroupCallRequest(
+                    call=types.InputGroupCall(id=-1001206327449,access_hash=0, ),join_as='ruhi',params=types.DataJSON(data='sed'
+                 ),
+                muted=True,
+                video_stopped=True,
+                ))
+
+
+
+                #group_call_factory = GroupCallFactory(client, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON)
+                #group_call = group_call_factory.get_file_group_call('input.raw')
+                #group_call.start(chat)
+                print('vc join sucess')
+            except Exception as e:
+                print('{e}')
+        print('sed lyuf')        
+    
